1e80cd683d9a56f2b9490701950b218c8e18612c
d3vnick <35925468+d3vnick@users.noreply.github.com>
2019-01-10 15:11:46 +0000

    Create JavascriptTextExtractor.js

---
+++ JavascriptTextExtractor.js
@@ -0,0 +1,6 @@
+var titles = new Set();
+document.querySelectorAll('div.fallback-text').forEach(el => {titles.add(el.textContent + '<br>')}); 
+html = "";
+titles.forEach(t => html += t)
+var win = window.open("", "Titles", "width=500,height=700");
+win.document.write(html);
